cefa362c432d89f6222941a05f6ad7f3f0b258a1
Drvulcanlord <122398995+Eli-Watson@users.noreply.github.com>
2024-05-01 21:53:41 -0500

    Add files via upload

---
+++ JS/DarkMode.js
@@ -0,0 +1,9 @@
+ function changeToBlack() {
+            document.body.style.backgroundColor = "#000";
+            document.body.style.color = "#fff";
+        }
+
+        function changeToWhite() {
+            document.body.style.backgroundColor = "#fff";
+            document.body.style.color = "#000";
+        }

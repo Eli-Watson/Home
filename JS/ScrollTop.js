b7c5efa12e09b9067182a626dbeb38ba8ce20691
Drvulcanlord <122398995+Eli-Watson@users.noreply.github.com>
2024-05-03 19:51:54 -0500

    Add files via upload

---
+++ JS/ScrollTop.js
@@ -0,0 +1,15 @@
+function scrollToTop() {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+}
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("scrollToTopBtn").style.display = "block";
+    } else {
+        document.getElementById("scrollToTopBtn").style.display = "none";
+    }
+}

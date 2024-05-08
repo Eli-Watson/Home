77f04129d164b5727f3916a2bfbb6691b19d3a56
Drvulcanlord <122398995+Eli-Watson@users.noreply.github.com>
2024-05-08 18:51:30 -0500

    Update NavHide.js

--- JS/NavHide.js
+++ JS/NavHide.js
@@ -1,8 +1,18 @@
 document.addEventListener("DOMContentLoaded", function() {
     const toggleNavButton = document.getElementById("toggleNav");
     const sideNav = document.querySelector("nav.sidenav");
+    const section = document.getElementById('section');
+    let isNavHidden = false;
 
     toggleNavButton.addEventListener("click", function() {
+        isNavHidden = !isNavHidden;
         sideNav.classList.toggle("hidden");
+        
+        if (isNavHidden) {
+            section.style.marginLeft = '0'; // Set margin when sidebar is hidden
+        } else {
+            section.style.marginLeft = '200px'; // Reset margin when sidebar is visible
+        }
     });
 });
+

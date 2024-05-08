179b5cc42014712702d6a49215df58fa01ca7860
Drvulcanlord <122398995+Eli-Watson@users.noreply.github.com>
2024-05-08 18:53:43 -0500

    Update NavHide.js

--- JS/NavHide.js
+++ JS/NavHide.js
@@ -4,15 +4,29 @@ document.addEventListener("DOMContentLoaded", function() {
     const section = document.getElementById('section');
     let isNavHidden = false;
 
-    toggleNavButton.addEventListener("click", function() {
+    // Function to check if the device is a mobile phone
+    function isMobileDevice() {
+        return (window.innerWidth <= 768); // Adjust the threshold as needed
+    }
+
+    // Function to toggle navigation sidebar and adjust section margin
+    function toggleNavAndMargin() {
         isNavHidden = !isNavHidden;
         sideNav.classList.toggle("hidden");
         
         if (isNavHidden) {
-            section.style.marginLeft = '0'; // Set margin when sidebar is hidden
+            section.style.marginLeft = '200px'; // Set margin when sidebar is hidden
         } else {
-            section.style.marginLeft = '200px'; // Reset margin when sidebar is visible
+            section.style.marginLeft = '0'; // Reset margin when sidebar is visible
         }
-    });
+    }
+
+    // Activate script if device is a mobile phone
+    if (isMobileDevice()) {
+        toggleNavAndMargin();
+    }
+
+    // Event listener for toggle button click
+    toggleNavButton.addEventListener("click", toggleNavAndMargin);
 });
 

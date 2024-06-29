e2db51bc03383b3e60360eadc7aa93d4727ecfa8
Drvulcanlord <122398995+Eli-Watson@users.noreply.github.com>
2024-06-29 10:56:58 -0500

    Update NavHide.js

--- JS/NavHide.js
+++ JS/NavHide.js
@@ -1,5 +1,5 @@
-// Function to toggle the visibility of the navigation bar and center the section
-function toggleNavBar() {
+
+ function toggleNavBar() {
     console.log("Toggle button clicked");
     // Get the navigation bar element
     var navBar = document.querySelector('nav.sidenav');
@@ -7,17 +7,18 @@ function toggleNavBar() {
     // Get the section element
     var section = document.querySelector('section');
 
-
     // Toggle the 'hidden' class to hide/show the navigation bar
     navBar.classList.toggle('hidden');
 
-    // Check if the navigation bar is hidden
-    if (navBar.classList.contains('hidden')) {
-        // If hidden, center the content by changing margin-left to auto
+    // Check if the navigation bar is hidden and the viewport is not mobile
+    if (navBar.classList.contains('hidden') && window.innerWidth > 768) {
+        // If hidden and not mobile, center the content by changing margin-left to auto
         section.style.marginLeft = 'auto';
-    } else {
-        // If not hidden, reset margin-left to 200px
+    } else if (window.innerWidth > 768) {
+        // If not hidden and not mobile, reset margin-left to 200px
         section.style.marginLeft = '200px';
+    } else {
+        // If on mobile, reset any inline styles
+        section.style.marginLeft = '';
     }
 }
- 

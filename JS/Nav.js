2d5b641a57d855279f7a74c4888a428643d6472b
Drvulcanlord <122398995+Eli-Watson@users.noreply.github.com>
2024-07-21 13:12:38 -0500

    Fix Nan.js
    finish

--- JS/Nav.js
+++ JS/Nav.js
@@ -4,11 +4,10 @@ document.addEventListener('DOMContentLoaded', function() {
         .then(data => {
             document.getElementById('Nav').innerHTML = data;
         });
-});
-document.addEventListener('DOMContentLoaded', function() {
     fetch('/Home/Pages/Footer.html')
         .then(response => response.text())
         .then(data => {
             document.getElementById('Footer').innerHTML = data;
         });
-});
+    });
+    

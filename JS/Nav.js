876304501b490933c1f03e6042af1f440dd1de2f
Drvulcanlord <122398995+Eli-Watson@users.noreply.github.com>
2024-07-21 12:47:48 -0500

    Footer
    Made the footer so that it is loaded dynamicly like the nav bar

--- JS/Nav.js
+++ JS/Nav.js
@@ -1,7 +1,14 @@
 document.addEventListener('DOMContentLoaded', function() {
-    fetch('/Home/Nav.html')
+    fetch('/Home/Pages/Nav.html')
         .then(response => response.text())
         .then(data => {
             document.getElementById('Nav').innerHTML = data;
+        });
+});
+document.addEventListener('DOMContentLoaded', function() {
+    fetch('/Home/Pages/Footer.html')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('Footer').innerHTML = data;
         });
 });

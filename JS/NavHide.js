a9deabde904cda631fbb919fa25d4df4d9642cf1
Drvulcanlord <122398995+Eli-Watson@users.noreply.github.com>
2024-07-26 17:26:20 -0500

    Massive update
    Added Color customization for both header and nav, addeds css to custimazation page, depreicated the modal for changing colors, revamped the nav hide, cleaned up code and added comments. This one took a while.

--- JS/NavHide.js
+++ JS/NavHide.js
@@ -1,5 +1,4 @@
 function toggleNavBar() {
-    console.log("Toggle button clicked");
 
     // Get the navigation bar element
     var navBar = document.querySelector('nav.sidenav');
@@ -10,36 +9,6 @@ function toggleNavBar() {
         return;
     }
 
-    // Get the section element
-    var section = document.querySelector('section');
-
-    // Get the footer element
-    var footer = document.querySelector('footer');
-
     // Toggle the 'hidden' class to hide/show the navigation bar
     navBar.classList.toggle('hidden');
-
-    // Check if the viewport is mobile (less than or equal to 768px)
-    if (window.innerWidth <= 768) {
-        // Always set margin-left to auto on mobile
-        section.style.marginLeft = 'auto';
-        if (footer) {
-            footer.style.marginLeft = 'auto';
-        }
-    } else {
-        // Check if the navigation bar is hidden or visible
-        if (navBar.classList.contains('hidden')) {
-            // If hidden, set margin-left to auto for both section and footer
-            section.style.marginLeft = 'auto';
-            if (footer) {
-                footer.style.marginLeft = 'auto';
-            }
-        } else {
-            // If not hidden, set margin-left to 150px for both section and footer
-            section.style.marginLeft = '150px';
-            if (footer) {
-                footer.style.marginLeft = '150px';
-            }
-        }
-    }
 }

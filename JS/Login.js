29e4ce69cfefefb70bfcf79b7af922801f33079c
Drvulcanlord <122398995+Eli-Watson@users.noreply.github.com>
2024-07-27 21:40:23 -0500

    update

--- JS/Login.js
+++ JS/Login.js
@@ -28,7 +28,7 @@ function checkLogin(event) {
                 return false;
         }
     } else {
-        alert("Invalid username or password. Mabey Don't guess this time.");
+        alert("Invalid username or password. Maby Don't guess this time.");
         return false;
     }
 
